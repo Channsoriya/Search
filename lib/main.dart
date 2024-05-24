81c42eec69ac9d0f57b222d19148eec2afe0f364
Channsoriya <broya.tk123@gmail.com>
2024-05-24 21:33:45 +0700

    Test

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 218, 6, 196)),
         useMaterial3: true,
       ),
       debugShowCheckedModeBanner: false,
