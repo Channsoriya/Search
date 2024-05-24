331cfe306cc8ba8c9d0f78bb16815156ff87d04c
Channsoriya <broya.tk123@gmail.com>
2024-05-24 22:25:28 +0700

    Test

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,6 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
         useMaterial3: true,
       ),
-      debugShowCheckedModeBanner: false,
       home: const Search()
     );
   }
